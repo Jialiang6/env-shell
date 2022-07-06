f5179db4ba995c78e55bc6354d6422ebbba1d6b7
Jialiang6 <86699853+Jialiang6@users.noreply.github.com>
2022-07-06 20:09:10 +0800

    Update ubuntu2deep.sh

--- ubuntu2deep.sh
+++ ubuntu2deep.sh
@@ -4,16 +4,23 @@
 cd /etc/apt/
 mv sources.list sources.list.bk
 cat >> sources.list << EOF
-deb http://mirrors.163.com/debian/ stretch main non-free contrib
-deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib
-deb http://mirrors.163.com/debian/ stretch-backports main non-free contrib
-deb-src http://mirrors.163.com/debian/ stretch main non-free contrib
-deb-src http://mirrors.163.com/debian/ stretch-updates main non-free contrib
-deb-src http://mirrors.163.com/debian/ stretch-backports main non-free contrib
-deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
-deb-src http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
+deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
 EOF
 # See other valid sources: <https://blog.csdn.net/whatday/article/details/105532784>
 
 # dependency and vim
-apt update && apt install -y net-tools --allow-remove-essential
+apt update
+apt install -y net-tools --allow-remove-essential
+apt remove vim-common
+apt remove openssh-client
+apt install -y vim
+apt install -y openssh-server
